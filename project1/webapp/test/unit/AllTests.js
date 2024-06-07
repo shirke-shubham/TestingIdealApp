4093482e9da9ced7d0e224b2bcfa920c26069ab4
shubham shirke <shubhamshirke100@gmail.com>
2024-06-07 08:37:40 +0000

    Commit-js file, cds, tables, procedure

---
+++ project1/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comdemo/project1/test/unit/controller/MasterPage.controller"
+], function () {
+	"use strict";
+});
---
+++ project1/webapp/test/unit/controller/MasterPage.controller.js
@@ -0,0 +1,16 @@
+/*global QUnit*/
+
+sap.ui.define([
+	"comdemo/project1/controller/MasterPage.controller"
+], function (Controller) {
+	"use strict";
+
+	QUnit.module("MasterPage Controller");
+
+	QUnit.test("I should test the MasterPage controller", function (assert) {
+		var oAppController = new Controller();
+		oAppController.onInit();
+		assert.ok(oAppController);
+	});
+
+});
